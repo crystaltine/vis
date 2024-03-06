5d46242a619303b31a01e2785d4cf80ac00116d4
trigtbh <lucas.varotto01@gmail.com>
2024-03-05 18:47:12 -0800

    register button!

--- viscord/registration.py
+++ viscord/registration.py
@@ -13,12 +13,21 @@ CONFIRM = ""
 import cursor
 cursor.hide()
 
-print(term.clear + term.move_yx(int(h * 0.5)-3, int(w/2) - int(len("username")/2)) + "USERNAME")
-print(term.move_yx(int(h * 0.5-2), int(w/2) - 2) + term.black_on_cyan + "____" + term.normal)
-print(term.move_yx(int(h * 0.5-1), int(w/2) - int(len("password")/2)) + "PASSWORD")
-print(term.move_yx(int(h * 0.5+0), int(w/2) - 2) + term.cyan + "____" + term.normal)
-print(term.move_yx(int(h * 0.5+1), int(w/2) - int(len("confirm password")/2)) + "CONFIRM PASSWORD")
-print(term.move_yx(int(h * 0.5+2), int(w/2) - 2) + term.cyan + "____" + term.normal)
+def clear():
+    print(term.clear)
+
+def update_screen(w, h, y): # honestly idk why either
+    clear()
+    print(term.clear + term.move_yx(int(h * 0.5)-3, int(w/2) - int(len("username")/2)) + "USERNAME")
+    update_user(w, h, y == 0)
+
+    print(term.move_yx(int(h * 0.5-1), int(w/2) - int(len("password")/2)) + "PASSWORD")
+    update_passwd(w, h, y == 1)
+    
+    print(term.move_yx(int(h * 0.5+1), int(w/2) - int(len("confirm password")/2)) + "CONFIRM PASSWORD")
+    update_confirm(w, h, y == 2)
+
+    update_button(w, h, y == 3)
 
 y = 0
 
@@ -55,8 +64,15 @@ def update_confirm(w, h, focused):
     else:
         print(term.move_yx(int(h * 0.5) +2, 0) + term.clear_eol + term.move_yx(int(h * 0.5+2), int(w/2 - len(t)/2)) + term.cyan + t + term.normal)
 
+def update_button(w, h, focused):
+    t = "[Register]"
+    if focused:
+        print(term.move_yx(int(h * 0.5) +4, 0) + term.clear_eol + term.move_yx(int(h * 0.5+4), int(w/2 - len(t)/2)) + term.black_on_lime + t + term.normal + term.move_yx(int(h * 0.5+2), int(w/2 - len(t)/2)))
+    else:
+        print(term.move_yx(int(h * 0.5) +4, 0) + term.clear_eol + term.move_yx(int(h * 0.5+4), int(w/2 - len(t)/2)) + term.normal + t + term.normal)
 
 
+update_screen(w, h, y)
 
 
 with term.cbreak():
@@ -65,7 +81,7 @@ with term.cbreak():
         h = term.height
         key = term.inkey(timeout=0.01)
         if key: 
-            if key.lower() in "qwertyuiopasdfghjklzxcvbnm":
+            if key.lower() in r"qwertyuiopasdfghjklzxcvbnm12345667890-=!@#$%^&*()_+[]{}":
                 if y == 0:
                     USER += key.lower()
                     update_user(w, h, True)
@@ -78,13 +94,16 @@ with term.cbreak():
 
             else:
                 code = key.code
-                if code == 258 and y < 2:
+                if code == 258 and y < 3:
                     if y == 0:
                         update_user(w, h, False)
                         update_passwd(w, h, True)
                     if y == 1:
                         update_passwd(w, h, False)
                         update_confirm(w, h, True)
+                    if y == 2:
+                        update_confirm(w, h, False)
+                        update_button(w, h, True)
                     y += 1
                 if code == 259 and y > 0:
                     if y == 2:
@@ -93,6 +112,9 @@ with term.cbreak():
                     if y == 1:
                         update_passwd(w, h, False)
                         update_user(w, h, True)
+                    if y == 3:
+                        update_button(w, h, False)
+                        update_confirm(w, h, True)
                     y -= 1
 
                 if code == 263:
@@ -103,9 +125,11 @@ with term.cbreak():
                         PASSWD = PASSWD[:-1]
                         update_passwd(w, h, True)
                     elif y == 2:
-                        CONFIRM = PASSWD[:-1]
+                        CONFIRM = CONFIRM[:-1]
                         update_confirm(w, h, True)
 
         else:
-            ...
-            # TODO: resize code
+            if lw != w or lh != h:
+                lw = w
+                lh = h
+                update_screen(w, h, y)
