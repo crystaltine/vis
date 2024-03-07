0dad580f46afe722323fd42890ea214fd10299a0
= <=>
2024-03-06 19:50:27 -0800

    Work on "messaging" UI #5

--- viscord/messages.py
+++ viscord/messages.py
@@ -2,13 +2,13 @@ import blessed
 import os, sys 
 
 
-chat_term = blessed.Terminal()
+message_term = blessed.Terminal()
 
 #prints out the height and width of the terminal
 #if the terminal is resized by hand, the vars will change value <- TO BE DELETED 
 
-height = chat_term.height 
-width = chat_term.width 
+height = message_term.height 
+width = message_term.width 
 print("width: " + str(width) + " height: " + str(height))
 
 
