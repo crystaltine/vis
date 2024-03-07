f09faaf9de29e23eebee83b455184c749726b2af
trigtbh <lucas.varotto01@gmail.com>
2024-03-07 13:33:45 -0800

    create some basic login flow

--- viscord/login.py
+++ viscord/login.py
@@ -7,9 +7,9 @@ h = term.height
 
 USER = ""
 PASSWD = ""
-CONFIRM = ""
 
 ERROR_MSG = ""
+BAD_PASSWORD = False
 
 import cursor
 cursor.hide()
@@ -25,13 +25,11 @@ def update_screen(w, h, y): # honestly idk why either
     print(term.move_yx(int(h * 0.5-1), int(w/2) - int(len("password")/2)) + "PASSWORD")
     update_passwd(w, h, y == 1)
     
-    print(term.move_yx(int(h * 0.5+1), int(w/2) - int(len("confirm password")/2)) + "CONFIRM PASSWORD")
-    update_confirm(w, h, y == 2)
 
-    update_button(w, h, y == 3)
+    update_button(w, h, y == 2)
 
     if ERROR_MSG:
-        print(term.move_yx(-1, 0) + term.clear_eol + term.move_yx(h-1, int(w/2 - len(ERROR_MSG)/2)) + term.black_on_red + ERROR_MSG + term.normal)
+        print(term.move_yx(y-1, 0) + term.clear_eol + term.move_yx(h-2, int(w/2 - len(ERROR_MSG)/2)) + term.black_on_red + ERROR_MSG + term.normal)
 
 y = 0
 
@@ -54,30 +52,27 @@ def update_passwd(w, h, focused):
     else:
         t = "*" * len(PASSWD)
     if focused:
-        print(term.move_yx(int(h * 0.5) - 0, 0) + term.clear_eol + term.move_yx(int(h * 0.5-0), int(w/2 - len(t)/2)) + term.black_on_cyan + t + term.normal)
+        if BAD_PASSWORD:
+            print(term.move_yx(int(h * 0.5) - 0, 0) + term.clear_eol + term.move_yx(int(h * 0.5-0), int(w/2 - len(t)/2)) + term.black_on_red + t + term.normal)
+        else:
+            print(term.move_yx(int(h * 0.5) - 0, 0) + term.clear_eol + term.move_yx(int(h * 0.5-0), int(w/2 - len(t)/2)) + term.black_on_cyan + t + term.normal)
     else:
-        print(term.move_yx(int(h * 0.5) - 0, 0) + term.clear_eol + term.move_yx(int(h * 0.5-0), int(w/2 - len(t)/2)) + term.cyan + t + term.normal)
-
-def update_confirm(w, h, focused):
-    if not CONFIRM:
-        t = "____"
-    else:
-        t = "*" * len(CONFIRM)
-    if focused:
-        print(term.move_yx(int(h * 0.5) +2, 0) + term.clear_eol + term.move_yx(int(h * 0.5+2), int(w/2 - len(t)/2)) + term.black_on_cyan + t + term.normal + term.move_yx(int(h * 0.5+2), int(w/2 - len(t)/2)))
-    else:
-        print(term.move_yx(int(h * 0.5) +2, 0) + term.clear_eol + term.move_yx(int(h * 0.5+2), int(w/2 - len(t)/2)) + term.cyan + t + term.normal)
+        if BAD_PASSWORD:
+            print(term.move_yx(int(h * 0.5) - 0, 0) + term.clear_eol + term.move_yx(int(h * 0.5-0), int(w/2 - len(t)/2)) + term.red + t + term.normal)
+        else:
+            print(term.move_yx(int(h * 0.5) - 0, 0) + term.clear_eol + term.move_yx(int(h * 0.5-0), int(w/2 - len(t)/2)) + term.cyan + t + term.normal)
 
 def update_button(w, h, focused):
-    t = "[Register]"
+    t = "[Log In]"
     if focused:
-        print(term.move_yx(int(h * 0.5) +4, 0) + term.clear_eol + term.move_yx(int(h * 0.5+4), int(w/2 - len(t)/2)) + term.black_on_lime + t + term.normal + term.move_yx(int(h * 0.5+2), int(w/2 - len(t)/2)))
+        print(term.move_yx(int(h * 0.5) +4, 0) + term.clear_eol + term.move_yx(int(h * 0.5+3), int(w/2 - len(t)/2)) + term.black_on_lime + t + term.normal + term.move_yx(int(h * 0.5+2), int(w/2 - len(t)/2)))
     else:
-        print(term.move_yx(int(h * 0.5) +4, 0) + term.clear_eol + term.move_yx(int(h * 0.5+4), int(w/2 - len(t)/2)) + term.lime + t + term.normal)
+        print(term.move_yx(int(h * 0.5) +4, 0) + term.clear_eol + term.move_yx(int(h * 0.5+3), int(w/2 - len(t)/2)) + term.lime + t + term.normal)
 
-def check_username():
+def check_creds(u, p):
     # TODO: actually access db to check if username exists or not
-    return True
+    import random
+    return random.choice([False, True]) # DELETE AFTER DONE
 
 update_screen(w, h, y)
 
@@ -94,69 +89,79 @@ with term.cbreak():
             if key.lower() in r"qwertyuiopasdfghjklzxcvbnm1234567890-=!@#$%^&*()_+[]{}":
                 if y == 0:
                     USER += key.lower()
+                    if BAD_PASSWORD and len(PASSWD) > 0:
+                        BAD_PASSWORD = False
+                        ERROR_MSG = ""
+                        update_passwd(w, h, False)
+                        print(term.move_yx(h-2, 0) + term.clear_eol)
                     update_user(w, h, True)
                 elif y == 1:
                     PASSWD += key
+                    if BAD_PASSWORD:
+                        BAD_PASSWORD = False
+                        ERROR_MSG = ""
+                        print(term.move_yx(h-2, 0) + term.clear_eol)
                     update_passwd(w, h, True)
-                elif y == 2:
-                    CONFIRM += key
-                    update_confirm(w, h, True)
 
             else:
                 code = key.code
-                if code in [258, 512, 343] and y < 3:
+                if code in [258, 512, 343] and y < 2:
                     if y == 0:
                         update_user(w, h, False)
                         update_passwd(w, h, True)
                     if y == 1:
                         update_passwd(w, h, False)
-                        update_confirm(w, h, True)
-                    if y == 2:
-                        update_confirm(w, h, False)
                         update_button(w, h, True)
                     y += 1
                 elif code in [259, 353] and y > 0:
                     if y == 2:
-                        update_confirm(w, h, False)
+                        update_button(w, h, False)
                         update_passwd(w, h, True)
                     if y == 1:
                         update_passwd(w, h, False)
                         update_user(w, h, True)
-                    if y == 3:
-                        update_button(w, h, False)
-                        update_confirm(w, h, True)
                     y -= 1
 
                 elif code == 263:
                     if y == 0:
                         USER = USER[:-1]
+                        if BAD_PASSWORD:
+                            BAD_PASSWORD = False
+                            ERROR_MSG = ""
+                            update_passwd(w, h, False)
+                            print(term.move_yx(h-2, 0) + term.clear_eol)
+
                         update_user(w, h, True)
                     elif y == 1:
+                        check = len(PASSWD) > 0
                         PASSWD = PASSWD[:-1]
+                        if BAD_PASSWORD and check:
+                            BAD_PASSWORD = False
+                            ERROR_MSG = ""
+                            print(term.move_yx(h-2, 0) + term.clear_eol)
                         update_passwd(w, h, True)
-                    elif y == 2:
-                        CONFIRM = CONFIRM[:-1]
-                        update_confirm(w, h, True)
 
-                elif code == 343 and y == 3:
+                elif code == 343 and y == 2:
                     ERROR_MSG = ""
                     if not USER:
                         ERROR_MSG = "[Username cannot be blank.]"
-                    elif not check_username():
-                        ERROR_MSG = "[User already exists.]"
                     elif not PASSWD:
                         ERROR_MSG = "[Password cannot be blank.]"
-                    elif PASSWD != CONFIRM:
-                        ERROR_MSG = "[Passwords do not match.]"
-                    
+                        BAD_PASSWORD = True
+                        update_passwd(w, h, False)
+                    elif not check_creds(USER, PASSWD):
+                        ERROR_MSG = "[Invalid credentials]"
+                        BAD_PASSWORD = True
+                        update_passwd(w, h, False)
+                    else:
+                        # TODO: remove later
+                        ERROR_MSG = "[Correct credentials, remove later]"
+
                     if ERROR_MSG:
                         print(term.move_yx(h-2, 0) + term.clear_eol + term.move_yx(h-2, int(w/2 - len(ERROR_MSG)/2)) + term.black_on_red + ERROR_MSG + term.normal + "\a")
                     else:
-                        import registered
-                        registered.show(USER)
-                        clear()
-                        exit()
-
+                        ...
+                        # TODO: later
 
         else:
             if lw != w or lh != h:
