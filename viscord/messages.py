ea268084d24725399dee0d890bad438183c15f2e
satishvaibhav <s-vsatish@lwsd.org>
2024-03-08 10:40:23 -0800

    testing, #5

--- viscord/messages.py
+++ viscord/messages.py
@@ -8,23 +8,22 @@ message_term = blessed.Terminal()
 
 height = message_term.height 
 width = message_term.width 
-counter = 0 
-counter2 = 0 
-b = True
-b2 = True
 
-#sets the background color of the terminal 
+#sets the background color of the terminal using 
+counter = 0 
+b = True
 while b:
     counter += 1
     print(message_term.on_gray21)
-    if counter == 300:
+    if counter == 30:
         b = False 
 
+counter2 = 0 
+b2 = True
 while b2:
     counter2 += 1
     print(message_term.on_gray21)
-    if counter2 == 300:
-        b2 = False
+    if counter2 == 50:
+        b2 = False 
 
-print(str(width))
-print(message_term.normal) 
+print(message_term.purple(str(width)))
