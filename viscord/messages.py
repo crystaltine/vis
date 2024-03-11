83715020b3434dca1217e2ddb706706bf573bd09
satishvaibhav <s-vsatish@lwsd.org>
2024-03-11 13:30:37 -0700

    Box created

--- viscord/messages.py
+++ viscord/messages.py
@@ -6,20 +6,18 @@ message_term = blessed.Terminal()
 print(message_term.clear)
 
 class MessageUI:
-    #Terminal Height 
+    #Terminal Height and Width 
     y = message_term.height 
     x = message_term.width 
-    #Box height,width,top,left
-    box_height = 40
-    box_width = 100
-    box_x = 5
-    box_y = 10 
-    #Draw Box 
-    print(message_term.move_yx())
-    #for i in box_height: 
-    print(message_term.move_yx())
-    
-    #print(message_term.purple(str(y)))
+    #Draw Box Method
+    def draw_a_box(box_height, box_width, box_x, box_y):
+        print(message_term.move_yx(box_y, box_x) + message_term.bold("┌") + message_term.bold("─") * (box_width - 2) + message_term.bold('┐'))
+        for i in range (1, box_height-1):
+            print(message_term.move_yx(box_y + i, box_x) + message_term.bold("│") + " " * (box_width - 2) + message_term.bold('│'))
+        print(message_term.move_yx(box_y + box_height - 1, box_x) + message_term.bold("└") + message_term.bold("─") * (box_width - 2) + message_term.bold('┘'))
+
+    draw_a_box(25, 100, 10, 20)
+    print(message_term.purple(str(y) + str(x)))
 
 """
 counter = 0 
