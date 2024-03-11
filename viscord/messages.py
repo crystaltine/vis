5bc48ddecc0925f5fc48756993fa48849f35b2a3
satishvaibhav <s-vsatish@lwsd.org>
2024-03-11 15:12:32 -0700

    #5, working

--- viscord/messages.py
+++ viscord/messages.py
@@ -10,13 +10,14 @@ class MessageUI:
     y = message_term.height 
     x = message_term.width 
     #Draw Box Method
-    def draw_a_box(box_height, box_width, box_x, box_y):
-        print(message_term.move_yx(box_y, box_x) + message_term.bold("┌") + message_term.bold("─") * (box_width - 2) + message_term.bold('┐'))
+    def draw_a_box(box_height, box_width, box_x_pos, box_y_pos, title):
+        print(message_term.move_yx(box_y_pos, box_x_pos) + message_term.bold("┌") + message_term.bold("─") * (box_width - 2) + message_term.bold('┐'))
         for i in range (1, box_height-1):
-            print(message_term.move_yx(box_y + i, box_x) + message_term.bold("│") + " " * (box_width - 2) + message_term.bold('│'))
-        print(message_term.move_yx(box_y + box_height - 1, box_x) + message_term.bold("└") + message_term.bold("─") * (box_width - 2) + message_term.bold('┘'))
+            print(message_term.move_yx(box_y_pos + i, box_x_pos) + message_term.bold("│") + " " * (box_width - 2) + message_term.bold('│'))
+        print(message_term.move_yx(box_y_pos - 17, box_x_pos + 44) + title)
+        print(message_term.move_yx(box_y_pos + box_height - 1, box_x_pos) + message_term.bold("└") + message_term.bold("─") * (box_width - 2) + message_term.bold('┘'))
 
-    draw_a_box(25, 100, 30, 20)
+    draw_a_box(25, 100, 30, 20, message_term.bold("Viscord Chat")) 
     print(message_term.purple(str(y) + str(x)))
 
 """
@@ -28,3 +29,4 @@ counter = 0
         if counter == 30:
             b = False 
 """
+    
