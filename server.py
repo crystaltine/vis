802d947423dd284a1837f6668c94b00497c0bb09
trigtbh <lucas.varotto01@gmail.com>
2024-03-12 13:41:35 -0700

    first implementation for server/db connection
    Co-authored-by: Akshai Srinivasan <akshaisrin@gmail.com>

--- server.py
+++ server.py
@@ -2,6 +2,25 @@ import socket
 import threading
 import json
 
+from uuid import uuid4
+import random
+import datetime
+
+import psycopg2
+
+ 
+
+conn_uri="postgres://avnadmin:AVNS_DyzcoS4HYJRuXlJCxuw@postgresql-terminal-suite-discord-terminal-suite-discord.a.aivencloud.com:15025/Discord?sslmode=require"
+
+def connect_to_db():
+    conn = psycopg2.connect(conn_uri)
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+
+    return cur
+
+cur = connect_to_db()
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("0.0.0.0", 5000))
 
@@ -20,8 +39,24 @@ def handle_message(data):
                 print(f"Error sending to {addr2}, removing...")
                 del connections[addr2]
 
+def handle_account_creation(data):
+    account_data = data["data"] # {"user": str (username), "password": str (password HASH)}
+    
+    uuid = str(uuid4())
+    symbol = random.choice("☀☁★☾♥♠♦♣♫☘☉☠")
+    color = "#ffffff"
+    timestamp = str(datetime.datetime.now())
+    user = account_data["user"]
+    password = account_data["password"]
+
+    send_query='''insert into "Discord"."UserInfo" (user_id, user_name, user_password, user_color, user_symbol, user_creation_timestamp) values (%s, %s, %s, %s, %s, %s)'''
+    cur.execute(send_query, (uuid, user, password, color, symbol, timestamp))
+
+
 handlers = {
-    "msg": handle_message
+    "msg": handle_message,
+    "account_create": handle_account_creation
+
 }
 
 def handle_connection(conn, addr):
@@ -43,6 +78,7 @@ def handle_connection(conn, addr):
                     parsed["from"] = addr
                     handlers[label](parsed)
                     break
+
 
 while True:
     s.listen()
