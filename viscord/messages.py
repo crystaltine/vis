4ab7b6ba6db89766a72f705feabd03dfcea11e63
satishvaibhav <s-vsatish@lwsd.org>
2024-03-11 20:10:40 -0700

    #5, created a proper box with a spot for the title

--- viscord/messages.py
+++ viscord/messages.py
@@ -9,24 +9,21 @@ class MessageUI:
     #Terminal Height and Width 
     y = message_term.height 
     x = message_term.width 
-    #Draw Box Method
+    #Draw Box Function 
     def draw_a_box(box_height, box_width, box_x_pos, box_y_pos, title):
-        print(message_term.move_yx(box_y_pos, box_x_pos) + message_term.bold("┌") + message_term.bold("─") * (box_width - 2) + message_term.bold('┐'))
+        #Top Border 
+        print(message_term.move_yx(box_y_pos, box_x_pos) + message_term.blue(message_term.bold("┌")) + 
+            message_term.blue(message_term.bold("─")) * (box_width - 2) + message_term.blue(message_term.bold("┐")))
+        #Left and Right Border 
         for i in range (1, box_height-1):
-            print(message_term.move_yx(box_y_pos + i, box_x_pos) + message_term.bold("│") + " " * (box_width - 2) + message_term.bold('│'))
+            print(message_term.move_yx(box_y_pos + i, box_x_pos) + message_term.blue(message_term.bold("│")) + message_term.on_gray21(" " * (box_width - 2)) + 
+                message_term.blue(message_term.bold('│'))) 
+        #Box Title 
         print(message_term.move_yx(box_y_pos - 17, box_x_pos + 44) + title)
-        print(message_term.move_yx(box_y_pos + box_height - 1, box_x_pos) + message_term.bold("└") + message_term.bold("─") * (box_width - 2) + message_term.bold('┘'))
-
-    draw_a_box(25, 100, 30, 20, message_term.bold("Viscord Chat")) 
-    print(message_term.purple(str(y) + str(x)))
-
-"""
-counter = 0 
-    b = True
-    while b:
-        counter += 1
-        print(message_term.on_gray21)
-        if counter == 30:
-            b = False 
-"""
+        #Bottom Border 
+        print(message_term.move_yx(box_y_pos + box_height - 1, box_x_pos) + message_term.blue(message_term.bold("└")) + message_term.blue(message_term.bold("─")) * 
+              (box_width - 2) + message_term.blue(message_term.bold('┘'))) 
+    #Draw the messages box 
+    box = draw_a_box(25, 100, 30, 20, message_term.green_on_gray21(message_term.bold("Viscord Chat"))) 
+    
     
