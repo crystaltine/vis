15ad9827e6bf56a9281df95510c5610b29f26a10
satishvaibhav <s-vsatish@lwsd.org>
2024-03-12 19:19:28 -0700

    had to resize box, #5

--- viscord/messages.py
+++ viscord/messages.py
@@ -17,12 +17,12 @@ class MessageUI:
             print(message_term.move_yx(box_y_pos + i, box_x_pos) + message_term.blue(message_term.bold("│")) + message_term.on_gray21(" " * (box_width - 2)) + 
                 message_term.blue(message_term.bold('│'))) 
         #Box Title 
-        print(message_term.move_yx(box_y_pos - 17, box_x_pos + 44) + title)
+        print(message_term.move_yx(box_y_pos - 18, box_x_pos + 32) + title)
         #Bottom Border 
         print(message_term.move_yx(box_y_pos + box_height - 1, box_x_pos) + message_term.blue(message_term.bold("└")) + message_term.blue(message_term.bold("─")) * 
               (box_width - 2) + message_term.blue(message_term.bold('┘'))) 
     #Draw the messages box 
-    box = draw_a_box(25, 100, 30, 20, message_term.green_on_gray21(message_term.bold("Viscord Chat"))) 
+    box = draw_a_box(25, 75, 30, 20, message_term.green_on_gray21(message_term.bold("Viscord Chat"))) 
     # Function which allows user to write in box 
     def write_in_box(text):
         with message_term.location(31, 19):
@@ -36,8 +36,8 @@ class MessageUI:
                 '\n' + write_in_box(line)
             if message_term.inkey() == '\x08':
                 line = line[:-1]
-            if len(line) > 100:
-                line_length = 98
+            if len(line) > 75:
+                line_length = 70
                 wrapped = textwrap.fill(line, line_length)
                 line = wrapped 
             write_in_box(line)
