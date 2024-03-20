2584dee3bc93abe6bb35c224deef4a5cf1a10e49
trigtbh <lucas.varotto01@gmail.com>
2024-03-20 15:51:16 -0700

    more testing - will need to create two separate sockets for better quality

--- viscord/voice_client_test.py
+++ viscord/voice_client_test.py
@@ -14,6 +14,11 @@ audio = pyaudio.PyAudio()
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("trigtbh.dev", 5000))
+
+
+s.setblocking(False)
+
+
 print("up!")
 
 input_stream = audio.open(format=FORMAT,
@@ -27,18 +32,26 @@ output_stream = audio.open(format=FORMAT,
                     rate=RATE,
                     output=True)
 
+input_volume = 200 # range from 0 to 200
+output_volume = 100
+
+dbfs_threshold = -50
 
 def handle_voice_io():
     while True:
         data = input_stream.read(CHUNK)
+        restructured = pydub.AudioSegment(data, sample_width=2, channels=1, frame_rate=RATE)
         if data:
-            # restructured = pydub.AudioSegment(data, sample_width=2, channels=1, frame_rate=RATE) + 10
-            # export = io.BytesIO()
-            s.sendall(data)
-        resp = s.recv(2048)
+            #print(restructured.dBFS)
+            if restructured.dBFS > dbfs_threshold:
+                restructured = restructured.apply_gain(pydub.utils.ratio_to_db(input_volume / 100))
+                s.sendall(restructured.raw_data)
+        try:
+            resp = s.recv(2048)
+        except BlockingIOError:
+            continue
         if resp:
             output_stream.write(resp)
-
 
 import threading
 threading.Thread(target=handle_voice_io).start()
