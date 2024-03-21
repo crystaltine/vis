4bc7b3c334806de41d1f77de90d7575b6ede39f3
trigtbh <lucas.varotto01@gmail.com>
2024-03-21 09:19:46 -0700

    undoing god-awful server changes

--- viscord/temp voice files/voice_server_2.py
+++ viscord/temp voice files/voice_server_2.py
@@ -22,38 +22,14 @@ connections = {}
 incoming_connections = {}
 outgoing_connections = {}
 
-voice_cache = {}
-
 no_voice_echo = False
 
-import pydub
-
-def rebroadcast_voice():
-    while True:
-        for channel in outgoing_connections:
-            if len(outgoing_connections[channel]) == 0:
-                continue
-            for conn, addr in outgoing_connections[channel]:
-                send_back = None
-                for conn2, addr2 in outgoing_connections[channel]:
-                    if (addr2 != addr or True) and addr2 in voice_cache: # TODO: remove later
-                        if send_back is None:
-                            send_back = pydub.AudioSegment(voice_cache[addr2], sample_width=2, channels=1, frame_rate=44100)
-                        else:
-                            send_back = send_back.overlay(pydub.AudioSegment(voice_cache[addr2], sample_width=2, channels=1, frame_rate=48000))
-                if send_back is not None:
-                    conn.sendall(send_back.raw_data)
-
-
-
 def handle_voice(conn, addr, data):
-    voice_cache[addr[0]] = data
-    print("New data")
-    # channel_id = incoming_connections.get(addr)
-    # if channel_id:
-    #     for conn2, addr2 in outgoing_connections[channel_id]:
-    #         if (addr2 != addr[0]):
-    #             conn2.sendall(data)
+    channel_id = incoming_connections.get(addr)
+    if channel_id:
+        for conn2, addr2 in outgoing_connections[channel_id]:
+            if not no_voice_echo or (addr2 != addr[0]):
+                conn2.sendall(data)
 
 def handle_outgoing_voice(conn, addr, data):
     if conn not in incoming_connections:
@@ -66,13 +42,9 @@ def handle_incoming_voice(conn, addr, data):
     outgoing_connections[data["channel_id"]].add((conn, addr[0]))
     conn.sendall(b"OK")
 
-def handle_ping(conn, addr, data):
-    conn.sendall(b"PONG")
-
 handlers = {
     "incoming-voice": handle_incoming_voice,
-    "outgoing-voice": handle_outgoing_voice,
-    "ping": handle_ping
+    "outgoing-voice": handle_outgoing_voice
 }
 
 def handle_connection(conn, addr):
@@ -104,8 +76,6 @@ def handle_connection(conn, addr):
                         handlers[label](conn, addr, parsed)
                         break
 
-
-threading.Thread(target=rebroadcast_voice).start()
 
 while True:
     s.listen()
