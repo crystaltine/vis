202b89ab36b06bf50313764ca04b6e6a782f7329
reshilbejoy <reshilbejoy@outlook.com>
2024-04-02 06:56:28 -0700

    backend push

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import threading
 import json
 import psycopg2
 import datetime
+import random
 
 conn_uri = "postgres://avnadmin:AVNS_DyzcoS4HYJRuXlJCxuw@postgresql-terminal-suite-discord-terminal-suite-discord.a.aivencloud.com:15025/Discord?sslmode=require"
 
@@ -21,23 +22,6 @@ print("Server up!")
 print("Running on " + str(s.getsockname()[0]) + ":" + str(s.getsockname()[1]))
 
 connections = {}
-
-def fetch_recent_messages(conn):
-    try:
-        send_query = """
-            SELECT mi.message_content, mi.message_timestamp, ui.user_name
-            FROM "Discord"."MessageInfo" AS mi
-            JOIN "Discord"."UserInfo" AS ui ON mi.user_id = ui.user_id
-            WHERE mi.server_id = %s
-            ORDER BY mi.message_timestamp DESC
-            LIMIT 15;
-        """
-        cur.execute(send_query, (conn,))
-        messages = cur.fetchall()
-        return messages
-    except Exception as e:
-        print("Error fetching messages:", e)
-        return []
 
 def handle_message(data, conn):
     send = json.dumps(data).encode()
@@ -89,14 +73,59 @@ def handle_login(data, conn):
     else:
         conn.sendall("False".encode("utf-8"))
 
-tokens = {}
+def handle_recent_messages(data, conn):
+    server_id = data["data"]
 
+    send_query = """
+    SELECT message_id, user_id, chat_id, server_id, replied_to_id, message_content, message_timestamp, pinged_user_ids
+    FROM "Discord"."MessageInfo"
+    WHERE server_id = %s
+    ORDER BY message_timestamp DESC
+    LIMIT 15
+    """
+    cur.execute(send_query, (server_id,))
+    messages = cur.fetchall()
+    messages_data = [{"message_id": msg[0], "user_id": msg[1], "chat_id": msg[2], "server_id": msg[3],
+                      "replied_to_id": msg[4], "message_content": msg[5], "message_timestamp": msg[6].isoformat(),
+                      "pinged_user_ids": msg[7]} for msg in messages]
+    conn.sendall(json.dumps(messages_data).encode("utf-8"))
+
+def handle_scroll_messages(data, conn):
+    scroll_data = data["data"]
+    server_id = scroll_data["server_id"]
+    current_position = scroll_data.get("current_position", 0)
+    direction = scroll_data["direction"]
+
+    if direction == "up":
+        current_position -= 1
+        if current_position < 0:
+            current_position = 0
+    else:  # direction == "down"
+        current_position += 1
+
+    send_query = """
+    SELECT message_id, user_id, chat_id, server_id, replied_to_id, message_content, message_timestamp, pinged_user_ids
+    FROM "Discord"."MessageInfo"
+    WHERE server_id = %s
+    ORDER BY message_timestamp DESC
+    OFFSET %s
+    LIMIT 15
+    """
+    cur.execute(send_query, (server_id, current_position))
+    messages = cur.fetchall()
+    messages_data = [{"message_id": msg[0], "user_id": msg[1], "chat_id": msg[2], "server_id": msg[3],
+                      "replied_to_id": msg[4], "message_content": msg[5], "message_timestamp": msg[6].isoformat(),
+                      "pinged_user_ids": msg[7]} for msg in messages]
+    conn.sendall(json.dumps({"messages": messages_data, "current_position": current_position}).encode("utf-8"))
+
+tokens = {}
 handlers = {
     "msg": handle_message,
     "account_create": handle_account_creation,
     "username_check": handle_username_check,
     "login": handle_login,
-    "get_recent_messages": fetch_recent_messages
+    "recent_messages": handle_recent_messages,
+    "scroll_messages": handle_scroll_messages
 }
 
 def handle_connection(conn, addr):
@@ -115,17 +144,12 @@ def handle_connection(conn, addr):
             parsed = json.loads(data.decode())
             for label in handlers:
                 if "type" in parsed and parsed["type"] == label:
-                    if label == "get_recent_messages":
-                        messages = handlers[label](addr)
-                        conn.sendall(json.dumps({"type": "recent_messages", "data": messages}).encode())
-                    else:
-                        parsed["from"] = addr
-                        handlers[label](parsed, conn)
+                    parsed["from"] = addr
+                    handlers[label](parsed, conn)
                     break
+print(handle_recent_messages(conn_uri))
 
 while True:
     s.listen()
     conn, addr = s.accept()
     threading.Thread(target=handle_connection, args=(conn, addr)).start()
-    print(fetch_recent_messages(conn))
-    print("zaza")
