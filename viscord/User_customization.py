1d64ad8c06e11d06d06cff7166b97a48b576fc18
satishvaibhav <s-vsatish@lwsd.org>
2024-04-03 18:02:49 -0700

    added change pasword ability

--- viscord/User_customization.py
+++ viscord/User_customization.py
@@ -26,8 +26,22 @@ def change_user_name(new_user_name):
             available_name_bool = False
             break 
     if available_name_bool: 
-        send_query = '''insert into "Discord"."UserInfo" (user_name) values (%s)'''
+        send_query = '''insert into "Discord"."UserInfo" (user_name = %s)'''
         cur.execute(send_query, (new_user_name))
+
+def change_user_password(new_user_password, user_name):
+    send_query='''select user_password FROM "Discord"."UserInfo" '''
+    cur.execute(send_query)
+    data = cur.fetchall()
+    available_password_bool = True
+    for password in data:
+        if new_user_password == password: 
+            print("Taken by Another User") 
+            available_password_bool = True
+            break 
+    if available_password_bool: 
+        send_query = '''insert into "Discord"."UserInfo" (user_password) values (%s) where (user_name = %s)'''
+        cur.execute(send_query, (new_user_password, user_name))
 
 def change_user_color(color):
         send_query = '''update "Discord"."UserInfo" (user_color = %s) where (user_name = %s)'''
