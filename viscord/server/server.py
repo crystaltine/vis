c03e81833ad89d47d09993e6469b0738442839c3
trigtbh <lucas.varotto01@gmail.com>
2024-04-16 13:02:43 -0700

    removed unnecessary text in server.py

--- viscord/server/server.py
+++ viscord/server/server.py
@@ -26,61 +26,12 @@ def handle_message(data, conn):
                 print(f"Error sending to {addr2}, removing...")
                 del connections[addr2]
 
-
-''' 
-def handle_recent_messages(data, conn):
-    server_id = data["data"]
-
-    send_query = """
-    SELECT message_id, user_id, chat_id, server_id, replied_to_id, message_content, message_timestamp, pinged_user_ids
-    FROM "Discord"."MessageInfo"
-    WHERE server_id = %s
-    ORDER BY message_timestamp DESC
-    LIMIT 15
-    """
-    cur.execute(send_query, (server_id,))
-    messages = cur.fetchall()
-    messages_data = [{"message_id": msg[0], "user_id": msg[1], "chat_id": msg[2], "server_id": msg[3],
-                      "replied_to_id": msg[4], "message_content": msg[5], "message_timestamp": msg[6].isoformat(),
-                      "pinged_user_ids": msg[7]} for msg in messages]
-    conn.sendall(json.dumps(messages_data).encode("utf-8"))
-
-def handle_scroll_messages(data, conn):
-    scroll_data = data["data"]
-    server_id = scroll_data["server_id"]
-    current_position = scroll_data.get("current_position", 0)
-    direction = scroll_data["direction"]
-
-    if direction == "up":
-        current_position -= 1
-        if current_position < 0:
-            current_position = 0
-    else:  # direction == "down"
-        current_position += 1
-
-    send_query = """
-    SELECT message_id, user_id, chat_id, server_id, replied_to_id, message_content, message_timestamp, pinged_user_ids
-    FROM "Discord"."MessageInfo"
-    WHERE server_id = %s
-    ORDER BY message_timestamp DESC
-    OFFSET %s
-    LIMIT 15
-    """
-    cur.execute(send_query, (server_id, current_position))
-    messages = cur.fetchall()
-    messages_data = [{"message_id": msg[0], "user_id": msg[1], "chat_id": msg[2], "server_id": msg[3],
-                      "replied_to_id": msg[4], "message_content": msg[5], "message_timestamp": msg[6].isoformat(),
-                      "pinged_user_ids": msg[7]} for msg in messages]
-    conn.sendall(json.dumps({"messages": messages_data, "current_position": current_position}).encode("utf-8"))
-'''
 tokens = {}
 handlers = {
     "msg": handle_message,
     "account_create": api.users.handle_account_creation,
     "username_check": api.users.handle_username_check,
     "login": api.login_flow.handle_login,
-#    "recent_messages": handle_recent_messages,
-#    "scroll_messages": handle_scroll_messages
     "token_bypass": api.login_flow.handle_token_bypass,
     "pin_message": api.messages.pin_message_endpoint,
     "handle_user_name_update": api.users.update_username_endpoint,
