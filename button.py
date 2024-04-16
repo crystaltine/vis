42cb21b268ed0716b116357ef967fe0fb3be3620
mshengehs <s-msheng@lwsd.org>
2024-04-16 12:14:36 -0700

    rm right and bottom styling opts, impl. partial rendering for div&text

--- button.py
+++ button.py
@@ -3,6 +3,7 @@ from utils import fcode, convert_to_chars
 from typing import TYPE_CHECKING, List, Unpack, Callable, Any
 from globalvars import Globals
 from logger import Logger
+from boundary import Boundary
 
 if TYPE_CHECKING:
     from key_event import KeyEvent
@@ -29,8 +30,6 @@ class Button(Element):
         top: str
         width: str
         height: str
-        right: str
-        bottom: str
         padding_top: int
         padding_right: int
         padding_bottom: int
@@ -48,8 +47,6 @@ class Button(Element):
         "top": "0%",
         "width": "100%",
         "height": "100%",
-        "right": None, # calculated from "left" and "width"
-        "bottom": None, # calculated from "top" and "height"
         "padding_top": 0,
         "padding_right": 0,
         "padding_bottom": 0,
@@ -66,10 +63,6 @@ class Button(Element):
     def __init__(self, **attrs: Unpack["Attributes"]):
         
         super().__init__(**attrs) # should ignore any unknown attributes that are provided
-        
-        # assert that ONE of left/right and ONE of top/bottom is provided
-        assert (self.style.get("left") is not None or self.style.get("right") is not None), "[Button]: At least one of left or right must not be None."
-        assert (self.style.get("top") is not None or self.style.get("bottom") is not None), "[Button]: At least one of top or bottom must not be None."
 
         self.on_pressed: Callable[[], Any] = attrs.get("on_pressed", lambda: None)
         """ Event handler to run when the button is pressed (enter is pressed down while it is hovered). Modifiable. """
@@ -83,29 +76,11 @@ class Button(Element):
         Globals.__vis_document__.selectable_elements.add(self)
         Globals.__vis_document__.hoverable_elements.add(self)
     
-    def render(self, container_left: int = None, container_top: int = None, container_right: int = None, container_bottom: int = None):
+    def render(self, container_bounds: Boundary):
 
         Logger.log(f"<BEGIN BUTTON render func>")
-        #Logger.log(f"Button render params: {container_left=} {container_top=} {container_right=} {container_bottom=}")
-        container_left, container_top, container_right, container_bottom = self.get_true_container_edges(container_left, container_top, container_right, container_bottom)
-        
-        container_width = container_right - container_left
-        container_height = container_bottom - container_top
-        
-        # Calculating client edges #############################################   
-        
-        self.client_top = container_top + (convert_to_chars(container_height, self.style.get("top")) if self.style.get("top") is not None
-            else container_bottom - convert_to_chars(container_height, self.style.get("bottom")) - convert_to_chars(container_height, self.style.get("height")))
-        self.client_bottom = (container_bottom - convert_to_chars(container_height, self.style.get("bottom")) if self.style.get("bottom") is not None
-            else container_top + convert_to_chars(container_height, self.style.get("top")) + convert_to_chars(container_height, self.style.get("height")))
-        
-        self.client_left = container_left + (convert_to_chars(container_width, self.style.get("left")) if self.style.get("left") is not None
-            else container_right - convert_to_chars(container_width, self.style.get("right")) - convert_to_chars(container_width, self.style.get("width")))
-        self.client_right = (container_right - convert_to_chars(container_width, self.style.get("right")) if self.style.get("right") is not None
-            else container_left + convert_to_chars(container_width, self.style.get("left")) + convert_to_chars(container_width, self.style.get("width")))
-        
-        self.client_width = self.client_right - self.client_left 
-        self.client_height = self.client_bottom - self.client_top
+        container_bounds = self.get_true_container_edges(container_bounds)
+        Boundary.set_client_boundary(self, container_bounds)
         
         #########################################################################
         
@@ -132,13 +107,13 @@ class Button(Element):
             
             if self is child: continue # ??? - for some reason using this func adds a self pointer to its own children. try fixing later
             
-            general_padding = convert_to_chars(container_width, self.style.get("padding")) if self.style.get("padding") is not None else 0
-            child.render(
-                self.client_left + convert_to_chars(container_width, self.style.get("padding_left")) or general_padding,
-                self.client_top + convert_to_chars(container_height, self.style.get("padding_top")) or general_padding,
-                self.client_right - convert_to_chars(container_width, self.style.get("padding_right")) or general_padding,
-                self.client_bottom - convert_to_chars(container_height, self.style.get("padding_bottom")) or general_padding
-            )
+            general_padding_x, general_padding_y = (convert_to_chars(self.client_width, self.style.get("padding")), convert_to_chars(self.client_height, self.style.get("padding"))) if self.style.get("padding") is not None else 0
+            child.render(Boundary(
+                self.client_left + convert_to_chars(self.client_width, self.style.get("padding_left")) or general_padding_x,
+                self.client_top + convert_to_chars(self.client_height, self.style.get("padding_top")) or general_padding_y,
+                self.client_right - convert_to_chars(self.client_width, self.style.get("padding_right")) or general_padding_x,
+                self.client_bottom - convert_to_chars(self.client_height, self.style.get("padding_bottom")) or general_padding_y
+            ))
              
     def add_child(self, child: "Element", index: int = None):
         """
--- text.py
+++ text.py
@@ -30,11 +30,11 @@ class Text(Element):
         italic: bool
         underline: bool
         left: int | None
-        right: int | None
         top: int | None
-        bottom: int | None
         width: int | None
         height: int | None
+        wrap: bool
+        text_align: Literal["left", "center", "right"]
     
     SUPPORTS_CHILDREN = False
     DEFAULT_STYLE: "Text.StyleProps" = {
@@ -46,12 +46,11 @@ class Text(Element):
         "italic": False,
         "underline": False,
         "left": 0,
-        "right": None, # defaults to 100% width
-        "width": "100%",
         "top": 0,
-        "bottom": None, # auto based on top & how many lines the text takes up
+        "width": "100%",
         "height": None, # auto based on text length
         "wrap": True,
+        "text_align": "left",
     }
     
     def __init__(self, **attrs: Unpack["Attributes"]):
@@ -59,46 +58,29 @@ class Text(Element):
         
         super().__init__(**attrs) # should ignore any unknown attributes that are provided
         self.text = attrs.get("text", "")
-        
-        # assert that not both left and right are None
-        assert (self.style.get("left") is not None or self.style.get("right") is not None), "[Text]: At least one of left or right must not be None."
-        # assert that not both top and bottom are None
-        assert (self.style.get("top") is not None or self.style.get("bottom") is not None), "[Text]: At least one of top or bottom must not be None."
-        
+
     def render(self, container_bounds: Boundary):
-        """
-        Renders the text to its container at the specified position, given the positions of the container.
-        """
 
         container_bounds = self.get_true_container_edges(container_bounds)
 
-        # calculate width-related attributes
-        l = self.style.get("left")
-        r = self.style.get("right")
+        # calculate width-related attributes 
         container_width = container_bounds.right - container_bounds.left
-        true_l = convert_to_chars(container_width, l)
-        true_r = convert_to_chars(container_width, r)
+        true_l = convert_to_chars(container_width, self.style.get("left"))
         true_w = convert_to_chars(container_width, self.style.get("width"))
-        self.client_left = container_bounds.left + (true_l if l is not None else container_bounds.right - true_r - true_w)
-        self.client_right = container_bounds.right - true_r if r is not None else container_bounds.left + true_l + true_w       
+        self.client_left = container_bounds.left + true_l
+        self.client_right = container_bounds.left + true_l + true_w       
         self.client_width = self.client_right - self.client_left 
         
-        # calculate height in a special way IF:
-        # top not None, bottom None, height None
-        # or bottom not None, top None, height None
-        # we assume client_width is fixed
+        # if height is none, then dynamically calculate based on how 
+        # many lines of text there are.
+        # if wrap is false, then theres just one line max (0 if empty text)
+        self.client_top = container_bounds.top + convert_to_chars(container_bounds.bottom - container_bounds.top, self.style.get("top"))
         if self.style.get("height") is None:
-            if self.style.get("top") is not None and self.style.get("bottom") is None:
-                self.client_top = container_bounds.top + convert_to_chars(container_bounds.bottom - container_bounds.top, self.style.get("top"))
-                # calculate height if wrap, otherwise just 1 line of text
-                self.client_height = (len(self.text) // self.client_width + 1) if self.style.get("wrap") else 1
-                self.client_bottom = self.client_top + self.client_height
-            elif self.style.get("bottom") is not None and self.style.get("top") is None:
-                self.client_bottom = container_bounds.bottom - convert_to_chars(container_bounds.bottom - container_bounds.top, self.style.get("bottom"))
-                # calculate height if wrap, otherwise just 1 line of text
-                self.client_height = (len(self.text) // self.client_width + 1) if self.style.get("wrap") else 1
-                self.client_top = self.client_bottom - self.client_height                
-                
+            self.client_height = (len(self.text) // self.client_width + 1) if self.style.get("wrap") else (1 if self.text else 0)              
+        else: # if height is explicitly defined then, well, yeah.
+            self.client_height = convert_to_chars(container_bounds.bottom-container_bounds.top, self.style.get("height"))
+        self.client_bottom = self.client_top + self.client_height 
+
         if not self.style.get("visible"): return
             
         style_string = " ".join([
@@ -107,5 +89,106 @@ class Text(Element):
             "underline" if self.style.get("underline") else ""
         ])
         
+        wrapped_text = [self.text]
+        if self.style.get("wrap") == True:
+            # wrap text based on client width
+            wrapped_text = [self.text[i*self.client_width:(i+1)*self.client_width] for i in range(len(self.text)//self.client_width+1)]
+
         with Globals.__vis_document__.term.hidden_cursor():
-            print(Globals.__vis_document__.term.move_xy(self.client_left, self.client_top) + fcode(self.style.get("color"), background=self.style.get("bg_color"), style=style_string) + self.text, end="")
+
+            text_chunk_index = 0 # which chunk of text to render on each line
+            for row in range(self.client_top, self.client_bottom):
+                text_chunk = wrapped_text[text_chunk_index] if text_chunk_index < len(wrapped_text) else ""
+                
+                # if the text chunk is not as long as the client width, then apply text_align
+                text_left_padding = 0 # 0 by default
+                if len(text_chunk) < self.client_width:
+                    text_left_padding = (
+                        (self.client_width - len(text_chunk))//2+1 if self.style.get("text_align") == "center"
+                        else self.client_width - len(text_chunk) if self.style.get("text_align") == "right"
+                        else 0
+                    )
+                
+                print(Globals.__vis_document__.term.move_xy(self.client_left, row) + fcode(self.style.get("color"), background=self.style.get("bg_color"), style=style_string) + text_left_padding*" " + text_chunk, end="")
+                text_chunk_index += 1
+
+    def _render_partial(self, container_bounds: Boundary, max_bounds: Boundary) -> None:
+        container_bounds = self.get_true_container_edges(container_bounds)
+
+        # calculate width-related attributes 
+        container_width = container_bounds.right - container_bounds.left
+        true_l = convert_to_chars(container_width, self.style.get("left"))
+        true_w = convert_to_chars(container_width, self.style.get("width"))
+        self.client_left = container_bounds.left + true_l
+        self.client_right = container_bounds.left + true_l + true_w       
+        self.client_width = self.client_right - self.client_left 
+        
+        # if height is none, then dynamically calculate based on how 
+        # many lines of text there are.
+        # if wrap is false, then theres just one line max (0 if empty text)
+        self.client_top = container_bounds.top + convert_to_chars(container_bounds.bottom - container_bounds.top, self.style.get("top"))
+        if self.style.get("height") is None:
+            self.client_height = (len(self.text) // self.client_width + 1) if self.style.get("wrap") else (1 if self.text else 0)              
+        else: # if height is explicitly defined then, well, yeah.
+            self.client_height = convert_to_chars(container_bounds.bottom-container_bounds.top, self.style.get("height"))
+        self.client_bottom = self.client_top + self.client_height 
+
+        if not self.style.get("visible"): return
+            
+        style_string = " ".join([
+            "bold" if self.style.get("bold") else "",
+            "italic" if self.style.get("italic") else "",
+            "underline" if self.style.get("underline") else ""
+        ])
+        
+        wrapped_text = [self.text]
+        if self.style.get("wrap") == True:
+            # wrap text based on client width
+            wrapped_text = [self.text[i*self.client_width:(i+1)*self.client_width] for i in range(len(self.text)//self.client_width+1)]
+
+        with Globals.__vis_document__.term.hidden_cursor():
+            for i in range(max(self.client_top, max_bounds.top), min(self.client_bottom, max_bounds.bottom)):
+            
+                # diagram:
+                #  cli_left   bounds_left              bounds_right  cli_right
+                #  |          |                        |             |
+                #  --------------------------------------------------- = client_width
+                #             -------------------------- = max_bounds.right - max_bounds.left
+                print(Globals.__vis_document__.term.move_xy(max(self.client_left, max_bounds.left), i) + self._bg_fcode + " " * min(self.client_width, max_bounds.right - max_bounds.left), end="")
+
+        # if completely out of render, just skip all this goofy ah garbage
+        if (
+            max_bounds.left > self.client_right or
+            max_bounds.right < self.client_left or 
+            max_bounds.top > self.client_bottom or
+            max_bounds.bottom < self.client_top
+        ): return
+
+        with Globals.__vis_document__.term.hidden_cursor():
+
+            text_chunk_index = 0 # which chunk of text to render on each line
+            for row in range(self.client_top, self.client_bottom):
+                
+                if max_bounds.bottom < row < max_bounds.top:
+                    text_chunk_index += 1
+                    continue
+
+                text_chunk = wrapped_text[text_chunk_index] if text_chunk_index < len(wrapped_text) else ""
+                
+                # if the text chunk is not as long as the client width, then apply text_align
+                text_left_padding = 0 # 0 by default
+                if len(text_chunk) < self.client_width:
+                    text_left_padding = (
+                        (self.client_width - len(text_chunk))//2+1 if self.style.get("text_align") == "center"
+                        else self.client_width - len(text_chunk) if self.style.get("text_align") == "right"
+                        else 0
+                    )
+                
+                text_to_render = text_left_padding*" " + text_chunk
+                if max_bounds.left > self.client_left: # cut off left side
+                    text_to_render = text_to_render[max_bounds.left-self.client_left:]
+                if max_bounds.right < self.client_right: # cut off right side
+                    text_to_render = text_to_render[:max_bounds.right-self.client_right]
+
+                print(Globals.__vis_document__.term.move_xy(self.client_left, row) + fcode(self.style.get("color"), background=self.style.get("bg_color"), style=style_string) + text_left_padding*" " + text_chunk, end="")
+                text_chunk_index += 1
