4f54fa1aa7b50eabd3ed276beb8f5d9d28528975
satishvaibhav <s-vsatish@lwsd.org>
2024-04-29 13:41:17 -0700

    started work on menu for GD

--- gd/GDMenu.py
+++ gd/GDMenu.py
@@ -1,1 +1,16 @@
+import blessed 
+
+Menuterm = blessed.Terminal()
+
+def draw_rect(width, height):
+    for y in range(height):
+        for x in range(width):
+            if y == 0 or y == height - 1 or x == 0 or x == width - 1:
+                print(Menuterm.on_blue2(Menuterm.blue2(' ')), end='')
+            else:
+                print(Menuterm.on_blue2(' '), end='')
+        print()
+    
+draw_rect(Menuterm.width, Menuterm.height)
+
 
