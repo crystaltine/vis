2ad0c3d21859a5ad1f2e20d8286051ec1f58e804
satishvaibhav <s-vsatish@lwsd.org>
2024-05-15 11:18:35 -0700

    #114 added background and basic buttons

--- gd/level_editor_menu.py
+++ gd/level_editor_menu.py
@@ -1,3 +1,7 @@
 import blessed 
 terminal = blessed.Terminal()
-
+from main import draw_menu_bg 
+from GDMenu import draw_main_menu_buttons
+class level_editor_menu: 
+    draw_menu_bg() 
+    draw_main_menu_buttons() 
