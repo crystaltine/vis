d79f7be8aeff1e4e63397f7f939b0495df540df9
satishvaibhav <s-vsatish@lwsd.org>
2024-05-16 10:00:04 -0700

    working on #114, progress is being made

--- gd/level_editor_menu.py
+++ gd/level_editor_menu.py
@@ -1,7 +1,32 @@
 import blessed 
 terminal = blessed.Terminal()
-from main import draw_menu_bg 
-from GDMenu import draw_main_menu_buttons
+from img2term.main import draw 
+from draw_utils import Position
+import os
+
+os.system('clear')
+
 class level_editor_menu: 
-    draw_menu_bg() 
-    draw_main_menu_buttons() 
+    def draw_menu_background(): 
+        draw('assets/menu_bg_1.png', Position.Relative(top=0, left=0), (terminal.width, terminal.height*2), 'scale')
+    def draw_play_button(): 
+            draw(f"assets/main_menu/start_button.png", pos=Position.Relative(left="calc(50% - 18ch)", bottom="calc(50% - 10ch)"))
+    def draw_menu_title(): 
+        left_pos = "calc(50% - 70)"
+        top_pos = 4 # arbitrary value
+        draw('assets/menu_title_1_editable.png', Position.Relative(left=left_pos, top=top_pos), (None, None), 'crop')
+
+
+
+
+
+#function calls 
+    draw_menu_background()
+    draw_menu_title() 
+
+    draw_play_button() 
+    
+
+    
+
+
