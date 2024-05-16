f10eee1db09add56841937c63519b315e624e3f8
satishvaibhav <s-vsatish@lwsd.org>
2024-05-16 13:29:06 -0700

    menu is working

--- gd/level_editor_menu.py
+++ gd/level_editor_menu.py
@@ -2,29 +2,45 @@ import blessed
 terminal = blessed.Terminal()
 from img2term.main import draw 
 from draw_utils import Position
+from GDMenu import _draw_start_button
 import os
 
 os.system('clear')
 
-class level_editor_menu: 
-    def draw_menu_background(): 
-        draw('assets/menu_bg_1.png', Position.Relative(top=0, left=0), (terminal.width, terminal.height*2), 'scale')
-    def draw_play_button(): 
-            draw(f"assets/main_menu/start_button.png", pos=Position.Relative(left="calc(50% - 18ch)", bottom="calc(50% - 10ch)"))
-    def draw_menu_title(): 
-        left_pos = "calc(50% - 70)"
-        top_pos = 4 # arbitrary value
-        draw('assets/menu_title_1_editable.png', Position.Relative(left=left_pos, top=top_pos), (None, None), 'crop')
+def draw_menu_background(): 
+    draw('assets/level_editor_menu.png', Position.Relative(top=0, left=0), (terminal.width, terminal.height*2), 'scale')
+
+def draw_menu_title(): 
+    left_pos = "calc(50% - 70)"
+    top_pos = 4 
+    draw('assets/level_menu_title_2.jpeg', Position.Relative(left=left_pos, top=top_pos), (None, None), 'crop')
+
+class Level_Editor_Menu: 
+    def create_menu(): 
+          
+          draw_menu_background()
+          draw_menu_title()
+          _draw_start_button() 
 
 
+          with terminal.hidden_cursor():
 
+            while True:
 
+                with terminal.cbreak():
+                    
+                    val = terminal.inkey(timeout=1)
 
+                    # Quitting game if q is hit
+
+                    if val == "q":
+                        os.system('cls')
+                        break        
 #function calls 
-    draw_menu_background()
-    draw_menu_title() 
+    create_menu()
+ 
 
-    draw_play_button() 
+   
     
 
     
