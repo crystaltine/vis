99d08ce80d0b1a207bb333399823508e6e00bd17
satishvaibhav <s-vsatish@lwsd.org>
2024-05-17 11:40:31 -0700

    working on the menu

--- gd/level_editor_menu.py
+++ gd/level_editor_menu.py
@@ -12,29 +12,38 @@ def draw_menu_background():
 def draw_menu_title(): 
     left_pos = "calc(50% - 70)"
     top_pos = 4 
-    draw('assets/level_menu_title_2.jpeg', Position.Relative(left=left_pos, top=top_pos), (None, None), 'crop')
+    draw('assets/level_editor_menu/level_menu_title_2.jpeg', Position.Relative(left=left_pos, top=top_pos), (None, None), 'crop')
 
 def draw_publish_button(): 
-    draw(f"assets/publish_button.png", pos=Position.Relative(right="calc(30% - 23ch)", bottom="calc(50% - 13ch)"))
+    draw(f"assets/level_editor_menu/publish_button_main.png", pos=Position.Relative(right="calc(30% - 23ch)", bottom="calc(50% - 13ch)"))
+
+def draw_create_button(): 
+      draw(f"assets/main_menu/create_button.png", pos=Position.Relative(left="calc(50% - 18ch)", bottom="calc(50% - 15ch)"))
+
+def draw_search_button(): 
+    return None
+
+def draw_my_levels_button(): 
+    return None 
+
+def draw_all_buttons(): 
+    draw_publish_button()
+    draw_create_button()
+    draw_search_button()
+    draw_my_levels_button() 
 
 class Level_Editor_Menu: 
     def create_menu(): 
           
           draw_menu_background()
           draw_menu_title()
-          draw_publish_button() 
-
+          draw_all_buttons()
 
           with terminal.hidden_cursor():
 
             while True:
-
                 with terminal.cbreak():
-                    
                     val = terminal.inkey(timeout=1)
-
-                    # Quitting game if q is hit
-
                     if val == "q":
                         os.system('cls')
                         break        
