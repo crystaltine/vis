af8c9660c55337c94002e85f754f182e27bb6afa
satishvaibhav <s-vsatish@lwsd.org>
2024-05-21 15:14:03 -0700

    finished Create UI for level editor menu #114

--- gd/level_editor_menu.py
+++ gd/level_editor_menu.py
@@ -7,8 +7,8 @@ import os
 os.system('clear')
 current_button_index=1 
 
-def draw_menu_background(): 
-    draw('assets/level_editor_menu/level_editor_menu.png', Position.Relative(top=0, left=0), (terminal.width, terminal.height*2), 'scale')
+def draw_menu_background(filename:str): 
+    draw(filename, Position.Relative(top=0, left=0), (terminal.width, terminal.height*2), 'scale')
 
 def draw_menu_title(): 
     left_pos = "calc(50% - 70)"
@@ -37,25 +37,25 @@ def draw_all_buttons(number):
         draw_search_button(False)
         draw_my_levels_button(False) 
         _draw_start_button(False)
-    elif number == 2: 
+    elif number == 4: 
         draw_create_button(True)
         draw_publish_button(False)
         draw_search_button(False)
         draw_my_levels_button(False) 
         _draw_start_button(False)
-    elif number == 3: 
+    elif number == 5: 
         draw_search_button(True)
         draw_publish_button(False)
         draw_create_button(False)
         draw_my_levels_button(False) 
         _draw_start_button(False)
-    elif number == 4: 
+    elif number == 2: 
         draw_my_levels_button(True) 
         draw_search_button(False)
         draw_publish_button(False)
         draw_create_button(False)
         _draw_start_button(False)
-    elif number == 5: 
+    elif number == 3: 
         _draw_start_button(True)
         draw_my_levels_button(False) 
         draw_search_button(False)
@@ -64,7 +64,7 @@ def draw_all_buttons(number):
     
 def create_menu(): 
           
-          draw_menu_background()
+          draw_menu_background('assets/level_editor_menu/level_editor_menu.png')
           draw_menu_title()
           draw_all_buttons(1) 
 
@@ -88,15 +88,23 @@ def handle_main_page_movement(val):
             changed=True
             current_button_index-=1
             if current_button_index<0:
-                current_button_index=2
+                current_button_index=5
 
         if val.name=='KEY_RIGHT':
             changed=True
             current_button_index+=1
-            if current_button_index>2:
-                current_button_index=0
+            if current_button_index>5:
+                current_button_index=1
 
-        if changed:
+        if val.name=='KEY_ENTER': 
+            draw_menu_background('assets/level_editor_menu/red_bg.png')
+        
+        if val.name == 'KEY_ESCAPE':
+          draw_menu_background('assets/level_editor_menu/level_editor_menu.png')
+          draw_menu_title()
+          draw_all_buttons(1) 
+
+        if changed and not val.name =='KEY_ENTER' and not val.name == 'KEY_ESCAPE':
             draw_all_buttons(current_button_index)
 
 create_menu()
