a391316d025456b8ca0c13587e6c3cdf9ae13699
satishvaibhav <s-vsatish@lwsd.org>
2024-05-23 13:45:01 -0700

    working on the main menu #123

---
+++ main-menu/MM.py
@@ -0,0 +1,9 @@
+from blessed import Terminal
+
+class MM:
+    """
+    General class for broad game elements.
+    Currently used for holding THE terminal instance, accessible from all parts of the game.
+    """
+    
+    term = Terminal()
