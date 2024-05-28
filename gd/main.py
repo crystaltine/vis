bebd594b73d1eb1a271a169a5c19cfcefa280084
arjunbalaji99 <arjunbalaji08@gmail.com>
2024-05-28 15:01:55 -0700

    autorestart, pausing, practicemode #116 #127 #128

--- gd/main.py
+++ gd/main.py
@@ -5,8 +5,12 @@ from GDMenu import draw_main_menu_buttons
 from level_selector import *
 import os 
 from img2term.main import draw
-from run_gd import run_level
+# from run_gd import run_level
 from logger import Logger
+from game import Game
+from parser import parse_level
+from copy import deepcopy
+
 
 terminal = blessed.Terminal()
 
@@ -14,6 +18,11 @@ main_current_index=1
 level_select_index=0
 pages=['main', 'LEVEL SELECTOR', 'level']
 current_page_index=0
+
+# currentgame stores the current attempt of a certain level within the game
+currentgame = None
+# the number of attempts currently tried within a certain level in the game
+attempt = 0
 
 def draw_menu_bg():
     """ This is a very crude function im writing at 5am ill make it better later """
@@ -33,6 +42,7 @@ def draw_menu_title():
 def main():
 
     global current_page_index
+    global currentgame
 
     init_main_page()
 
@@ -58,9 +68,24 @@ def main():
                     os.system('cls')
                     break
                 
-                if current_page_index>0 and val.name=="KEY_ESCAPE":
+                # handles exit code from level selector or exiting from pause menu within the level
+                if ((current_page_index>0 and current_page_index < 2 and val.name=="KEY_ESCAPE") or (currentgame != None and current_page_index == 2 and currentgame.exiting)) :
+                    # unsets the current game as the attempt is now complete
+                    currentgame = None
                     current_page_index-=1
                     render_new_page()
+                # handles resetting the game (occurs after each death)
+                if ((currentgame != None and current_page_index == 2 and currentgame.reseting)):
+                    practice_mode = False
+                    checkpoints = []
+                    # if the game was in practice mode, saves the checkpoints that were stored in the previous attempt
+                    if currentgame.practice_mode:
+                        practice_mode = True
+                        if currentgame.checkpoints:
+                            checkpoints = deepcopy(currentgame.checkpoints)
+                    currentgame = None
+                    # runs the level with the necessary paramaters based on if it is in practice mode or not
+                    run_level(levels[level_select_index]['path'], practice_mode, checkpoints)
 
                 call_handle_page_function(val)
 
@@ -89,6 +114,8 @@ def init_main_page():
     draw_main_menu_buttons(1)
  
 def init_level_selector():
+    global attempt
+
     # draw fullscreen bg
     draw_rect("#000000", Position.Relative(0, 0, 0, 0))
 
@@ -101,6 +128,8 @@ def init_level_selector():
     draw_spike(int(terminal.width*0.025), int(terminal.width*0.03), int(terminal.height*0.5), 'white', 'left')
 
     level_info=levels[0]
+    # resets the attempt number when returning to the level selector screen
+    attempt = 0
     draw_level(level_info['level_name'], level_info['level_description'], int(terminal.width*0.8), int(terminal.height*0.6), 
                    int(terminal.width*0.1), int(terminal.height*0.3), level_info['color1'], level_info['color2'])
 
@@ -167,7 +196,7 @@ def handle_level_select_page(val):
     if val.name=='KEY_ENTER':
         current_page_index=2
         
-    run_level(levels[level_select_index]['path'])
+        run_level(levels[level_select_index]['path'])
 
     # If a button has been pressed, reset the level, and regenerate the new level onto the screen
 
@@ -177,6 +206,43 @@ def handle_level_select_page(val):
         reset_level()
         draw_level(level_info['level_name'], level_info['level_description'], int(terminal.width*0.8), int(terminal.height*0.6), 
                    int(terminal.width*0.1), int(terminal.height*0.3), level_info['color1'], level_info['color2'])
+
+def run_level(path: str, practice_mode: bool = False, checkpoints: list[tuple[float, float]] = None) -> None:
+    """
+    Runs the specified level from the given path utilizing the given checkpoints (if any).
+
+    Args:
+        path (str): The path to the level file to be run. (e.g. "levels/level1.txt")
+        practice_mode (bool): If True, the level will be run in practice mode. Defaults to False.
+        checkpoints (list[tuple[float, float]]): A list of checkpoint coordinates (for practice mode), each checkpoint is a tuple of (x, y) positions. Defaults to None.
+    """
+    global currentgame
+    global attempt
+
+    leveldata = path
+    if isinstance(leveldata, str):
+        leveldata = parse_level(leveldata)
+        for row in leveldata:
+            Logger.log(f"Level row types: {[type(row_obj) for row_obj in row]}")
+
+    currentgame = Game(leveldata)
+    # increments the attempt number
+    attempt += 1
+    currentgame.attempt = attempt
+
+    # if the level is in practice mode, then it sets it as so
+    if practice_mode:
+        currentgame.practice_mode = True
+        # if the level has existing checkpoints, it adds them into the current game
+        # additionally, it sets the most recent checkpoint, and the player position there
+        # this is so that in practice mode the player will respawn at the most recent checkpoint
+        if checkpoints:
+            currentgame.checkpoints = checkpoints
+            currentgame.last_checkpoint = checkpoints[-1]
+            currentgame.player.pos = [currentgame.last_checkpoint[0], currentgame.last_checkpoint[1]]
+
+    currentgame.start_level()
+
 
 if __name__ == "__main__":
     # testing
---
+++ gd/pausemenu.py
@@ -0,0 +1,6 @@
+from draw_utils import draw_rect, Position, convert_to_chars
+from img2term.main import draw
+import os
+
+os.system('clear')
+
