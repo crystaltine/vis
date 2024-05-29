231fbd9a48b4a36f698a7925271727cd14c1f49f
akshaisrin <akshaisrin@gmail.com>
2024-05-29 11:48:44 -0700

    Fixed bugs

--- gd/created_levels.py
+++ gd/created_levels.py
@@ -23,8 +23,11 @@ def update_list_of_level_files(file_type='.level'):
             
             if file.endswith(file_type):
                 # print whole path of files
-                level_file_names.append(os.path.join(root, file))
-                logging.error(level_file_names)
+                filename=os.path.join(root, file)
+                #logging.error('name   ' + filename)
+                filename=filename[filename.index('\\')+1:]
+                level_file_names.append(filename)
+                
 
     if len(level_file_names)==0:
         level_file_names.append('You have not created any levels')
@@ -47,7 +50,9 @@ def draw_created_levels():
         text=level_path
     else:
         color=random.choice(colors)
-        draw_created_level(level_path[level_path.index('\\')+1:level_path.index('.level')], int(GD.term.width*0.8), int(GD.term.height*0.6), int(GD.term.width*0.1), int(GD.term.height*0.3), color[0], color[1])
+        level_name=level_path[0:level_path.index('.')]
+        level_name=level_path[0].upper()+level_name[1:]
+        draw_created_level(level_name, int(GD.term.width*0.8), int(GD.term.height*0.6), int(GD.term.width*0.1), int(GD.term.height*0.3), color[0], color[1])
     
     while True:
         draw_text(text, int(terminal.width*0.4), int(terminal.height*0.5))
@@ -62,16 +67,15 @@ def init_created_levels_page(terminal):
     draw_rect("#000000", Position.Relative(0, 0, 0, 0))
 
     level_path=level_file_names[0]
-    color=random.choice(colors)
-
+    color=colors[0]
     if level_path=='You have not created any levels':
         draw_text(level_path, int(terminal.width*0.4), int(terminal.height*0.5))
     else:
-        selector_text='Select a level by navigating with the arrow keys and hitting enter once you have chosen a level'
+        selector_text='Select a level by navigating with the arrow keys. Hit p to play a level and e to edit.'
         draw_text(selector_text, int(terminal.width*0.23), int(terminal.height*0.2))
         draw_spike(int(terminal.width*0.025), int(terminal.width*0.95), int(terminal.height*0.5), 'white', 'right')
         draw_spike(int(terminal.width*0.025), int(terminal.width*0.03), int(terminal.height*0.5), 'white', 'left')
-        draw_created_level(level_path[level_path.index('\\')+1:level_path.index('.level')], int(terminal.width*0.8), int(terminal.height*0.6), 
+        level_name=level_path[0:level_path.index('.')]
+        level_name=level_path[0].upper()+level_name[1:]
+        draw_created_level(level_name, int(terminal.width*0.8), int(terminal.height*0.6), 
                    int(terminal.width*0.1), int(terminal.height*0.3), color[0], color[1])
-    
-#draw_level_selector()
