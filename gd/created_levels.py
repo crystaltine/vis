679710bcdedd23c96ff5fd672f4dc7df57bcd5aa
akshaisrin <akshaisrin@gmail.com>
2024-05-29 23:13:57 -0700

    Fixed another merge bug

--- gd/created_levels.py
+++ gd/created_levels.py
@@ -50,6 +50,9 @@ def draw_created_levels():
         level_name=level_path[level_path.index('\\')+1:level_path.index('.level')]
         level_name=level_name[0].upper()+level_name[1:]
         draw_created_level(level_name, int(GD.term.width*0.8), int(GD.term.height*0.6), int(GD.term.width*0.1), int(GD.term.height*0.3), color[0], color[1])
+    
+    while True:
+        draw_text(text, int(terminal.width*0.4), int(terminal.height*0.5))
 
 def reset_level(width=int(GD.term.width*0.8), height=int(GD.term.height*0.6), x=int(GD.term.width*0.1), y=int(GD.term.height*0.3)):
     draw_rect('black', Position.Relative(left=x, top=y), width, height)
@@ -61,7 +64,7 @@ def init_created_levels_page(terminal):
     draw_rect("#000000", Position.Relative(0, 0, 0, 0))
 
     level_path=level_file_names[0]
-    color=random.choice(colors)
+    color=colors[0]
 
     if level_path=='You have not created any levels':
         draw_text(level_path, int(terminal.width*0.4), int(terminal.height*0.5))
@@ -70,7 +73,7 @@ def init_created_levels_page(terminal):
         draw_text(selector_text, int(terminal.width*0.23), int(terminal.height*0.2))
         draw_spike(int(terminal.width*0.025), int(terminal.width*0.95), int(terminal.height*0.5), 'white', 'right')
         draw_spike(int(terminal.width*0.025), int(terminal.width*0.03), int(terminal.height*0.5), 'white', 'left')
-        draw_created_level(level_path[level_path.index('\\')+1:level_path.index('.level')], int(terminal.width*0.8), int(terminal.height*0.6), 
+        level_name=level_path[level_path.index('\\')+1:level_path.index('.level')]
+        level_name=level_name[0].upper()+level_name[1:]
+        draw_created_level(level_name, int(terminal.width*0.8), int(terminal.height*0.6), 
                    int(terminal.width*0.1), int(terminal.height*0.3), color[0], color[1])
-    
-#draw_level_selector()
