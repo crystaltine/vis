82dd299bd4d322c4853cec369387ada9e4da44dc
trigtbh <lucas.varotto01@gmail.com>
2024-05-29 21:24:08 -0700

    basic messages ui????

--- viscord/client/messages.py
+++ viscord/client/messages.py
@@ -8,6 +8,8 @@ import requests
 import config
 import registry
 import sys
+import json
+import multiprocessing as mp
 
 import socket
 
@@ -21,8 +23,11 @@ global typed_message
 typed_message = ""
 
 def draw_typed_message():
-    print(term.move(int(term.width * 0.1 + 1), int(term.height * 0.9) - 1) + term.on_color_rgb(*hex_to_rgb(colors.field)) + term.color_rgb(*hex_to_rgb(colors.text)) + typed_message + " " * (int(term.width * 0.8 - 1)))
+    print(term.move(int(term.height * 0.9 - 2), int(term.width * 0.1) + 1) + term.on_color_rgb(*hex_to_rgb(colors.field)) + term.color_rgb(*hex_to_rgb(colors.text)) + " " * (int(term.width * 0.8 - 2)))
 
+    print(term.move(int(term.height * 0.9 - 4), int(term.width * 0.1)) + term.on_color_rgb(*hex_to_rgb(colors.div_shadow)) + term.color_rgb(*hex_to_rgb(colors.div_shadow)) + " " * (int(term.width * 0.8)))
+
+    
 
 
 def hex_to_rgb(hex):
@@ -42,8 +47,32 @@ def draw_menu():
     for y in range(tly, tly + int(term.height * 0.8)):
         print(term.move(y, tlx) + term.on_color_rgb(*hex_to_rgb(colors.div)) + ' ' * int(term.width * 0.8), end="")
 
+    print(term.move(tly-1, tlx-1) + term.on_color_rgb(*hex_to_rgb(colors.background)) + term.color_rgb(*hex_to_rgb(colors.div_shadow)) + "▄" * int(term.width * 0.8 + 2), end="")
+    print(term.move(tly + int(term.height * 0.8), tlx-1) + term.on_color_rgb(*hex_to_rgb(colors.background)) + term.color_rgb(*hex_to_rgb(colors.div_shadow)) + "▀" * int(term.width * 0.8 + 2), end="")
+    for y in range(tly, tly + int(term.height * 0.8)):
+        print(term.move(y, tlx-1) + term.on_color_rgb(*hex_to_rgb(colors.div_shadow)) + " ", end="")
+        print(term.move(y, tlx + int(term.width * 0.8)) + term.on_color_rgb(*hex_to_rgb(colors.div_shadow)) + " ", end="")
+
+def socket_handler():
+    global s, data
+    try:
+        while True:
+            data = s.recv(4096)
+            if not data:
+                break
+            message_data = json.loads(data.decode())
+            data.append(message_data)
+            show_recent_messages()
+    except Exception as e:
+        pass
+    
+
 def show_recent_messages():
-    ...
+    tly = int(term.height * 0.1)
+    tlx = int(term.width * 0.1)
+
+    for y in range(tly, tly + int(term.height * 0.8) - 4):
+        print(term.move(y, tlx) + term.on_color_rgb(*hex_to_rgb(colors.div)) + " " * (int(term.width * 0.8)))
 
 
 def redraw_all():
@@ -51,6 +80,7 @@ def redraw_all():
     draw_menu()
 
     draw_typed_message()
+    show_recent_messages()
 
     print("")
 
@@ -68,19 +98,18 @@ def main(user_token, server_id, channel_id):
 
         resp = requests.post(f"{config.API_URL}/api/messages/get_recent", json={
             "chat_id": channel_id,
+            "server_id": server_id,
             "user_token": user_token,
             "num": 15
         })
         if resp.status_code != 200:
-            print(resp.status_code)
-            input()
+            return
         data = resp.json()["data"]
     except Exception as e:
-        print(term.clear)
-        print(traceback.format_exc(), flush=True)
-        input()
-        raise
+        return
 
+    proc = mp.Process(target=socket_handler)
+    proc.start()
 
     redraw_all()
     with term.cbreak():
@@ -95,5 +124,6 @@ def main(user_token, server_id, channel_id):
                 continue
             if val.code == term.KEY_ESCAPE:
                 s.close()
+                proc.terminate()
                 break
             
