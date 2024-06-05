dd11fecbe5d27ff296fedbebdb0f9b508431bed9
trigtbh <lucas.varotto01@gmail.com>
2024-06-05 11:13:06 -0700

    replacing dicts with wrapped dict class

--- viscord/server/api/voice_client.py
+++ viscord/server/api/voice_client.py
@@ -15,7 +15,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, VOICE_PORT))
 
 
-global channels, lifelines, connected_clients
 
 connected_clients = {
     # user id: {
@@ -27,12 +26,38 @@ channels = {}
 
 lifelines = {}
 
+
+
+class GlobalState:
+    def __init__(self):
+        self._channels = {}
+        self._lifelines = {}
+        self._connected_clients = {}
+
+    @property
+    def channels(self):
+        return self._channels
+
+    @property
+    def lifelines(self):
+        return self._lifelines
+
+    @property
+    def connected_clients(self):
+        return self._connected_clients
+
+global_state = GlobalState()
+
 lock = threading.RLock()
 
 
 @app.route("/api/voice/join", methods=["POST"])
 def join_voice() -> Literal["success", "failure"]:
-    global channels, lifelines, connected_clients
+
+    channels = global_state.channels
+    lifelines = global_state.lifelines
+    connected_clients = global_state.connected_clients
+
     """
     Join a voice channel.
     """
@@ -64,13 +89,17 @@ def join_voice() -> Literal["success", "failure"]:
         data = {"msg": "join", "chat_id": chat_id, "id": user_id}
         for uid in channels[chat_id]:
             if uid != user_id:
-                with lock:
-                    lifelines[uid].send(json.dumps(data).encode())
+                print(hash(uid))
+                lifelines[uid].send(json.dumps(data).encode())
 
     return Response(json.dumps(return_data), status=200)
 
 def handle_client(conn, addr):
-    global channels, lifelines, connected_clients
+    channels = global_state.channels
+    lifelines = global_state.lifelines
+    connected_clients = global_state.connected_clients
+    
+    
     data = conn.recv(1024)
     data = json.loads(data.decode())
     
@@ -83,9 +112,9 @@ def handle_client(conn, addr):
         connected_clients[target][user_id] = conn
         print(f"NEW RECEIVER: {target} -> {user_id}")
     elif role == "lifeline":
-        with lock:
-            lifelines[user_id] = conn
-        print(f"NEW LIFELINE: {user_id}")
+        lifelines[user_id] = conn
+        print(f"NEW LIFELINE: {user_id} ({hash(user_id)})")
+        print()
     elif role == "sender":
         if user_id not in connected_clients:
             connected_clients[user_id] = {}
@@ -94,9 +123,10 @@ def handle_client(conn, addr):
     while True:
         try:
             data = conn.recv(2048)
-        except:
+        except Exception as e:
             ...
             # TODO: handle disconnect - prioritize lifelines first
+            print(e)
             return
         if role == "sender":
             if user_id not in connected_clients: continue
